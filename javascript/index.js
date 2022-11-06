a32abaf7db1f336f75b6473a9292bec07b9c2330
Viviana <vivianacg8@gmail.com>
2022-11-06 14:01:20 -0600

    Added multiple locations times

---
+++ javascript/index.js
@@ -0,0 +1,53 @@
+function updateTime() {
+  // Seoul
+  let seoulElement = document.querySelector("#seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
+
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  //Cancun
+  let cancunElement = document.querySelector("#cancun");
+  if (cancunElement) {
+    let cancunDateElement = cancunElement.querySelector(".date");
+    let cancunTimeElement = cancunElement.querySelector(".time");
+    let cancunTime = moment().tz("America/Cancun");
+
+    cancunDateElement.innerHTML = cancunTime.format("MMMM Do YYYY");
+    cancunTimeElement.innerHTML = cancunTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small>
+        </div>
+    </div>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
